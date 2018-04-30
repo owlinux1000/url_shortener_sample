e49ebcf0c351c04860612c1859c160b9e8329c7b
encry1024 <register.chihiro@gmail.com>
2018-04-30 16:28:25 +0900

    Change random string length

--- main.rb
+++ main.rb
@@ -32,16 +32,16 @@ post '/add' do
   if url.nil? || url == ""
     return json({"code": 1, "message": "No url"})
   end
-
+  
   logger.info(url)
 
   r = $redis.get(url)
-  
+
   unless r.nil?
     return r
   end
 
-  random_string = SecureRandom.urlsafe_base64(8)
+  random_string = SecureRandom.urlsafe_base64(4)
 
   $redis.sadd(random_string, url)
   t = Time.now + 60*60*24*3
