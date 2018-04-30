04b783aa41b6455ad5cb32399f8f0a13fdd1d7f1
encry1024 <register.chihiro@gmail.com>
2018-04-30 15:38:21 +0900

    Update

--- main.rb
+++ main.rb
@@ -3,23 +3,40 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/json'
+require 'sinatra/cross_origin'
 require "securerandom"
 require 'redis'
 require 'logger'
+require 'json'
+require 'uri'
 
 logger = Logger.new('sinatra.log')
 $redis = Redis.new
 
-post '/' do
+options '*' do
+  headers 'Access-Control-Allow-Origin' => 'http://localhost:8080',
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+          'Access-Control-Allow-Headers' => 'Content-Type'
+end
+
+before do
+  headers 'Access-Control-Allow-Origin' => 'http://localhost:8080'
+end
+
+post '/add' do
+
+  params = JSON.parse(request.body.read)
+  url = params["url"]
   
-  url = params[:url]
   if url.nil? || url == ""
-    return "No url"
+    return json({"code": 1, "message": "No url"})
   end
-  
+
   logger.info(url)
 
   r = $redis.get(url)
+  
   unless r.nil?
     return r
   end
@@ -27,8 +44,15 @@ post '/' do
   random_string = SecureRandom.urlsafe_base64(8)
 
   $redis.sadd(random_string, url)
-  $redis.sadd(random_string, Time.now + 60*60*24*3)
-  return "http://localhost:4567/" + random_string
+  t = Time.now + 60*60*24*3
+  $redis.sadd(random_string, t)
+
+  json({
+        "code": 0,
+        "message": "success",
+        "url": "http://localhost:4567/" + random_string,
+        "time": t
+       })
   
 end
 
@@ -38,14 +62,14 @@ get '/:random_string' do
   r = $redis.smembers(random_string)
   
   if r[1].nil?
-    return "Invalid url"
+    return json({"code": 1, "message": "Invalid url"})
   end
   
-  if (r[0] - Time.now <= 0)
+  if (Time.parse(r[1]) - Time.now <= 0)
     $redis.del(random_string)
-    return "Expired"
+    return json({"code": 1, "message": "Expired"})    
   end
   
-  redirect to($redis.get(random_string))
+  redirect to(r[0])
   
 end
